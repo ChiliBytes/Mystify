93544989c8108dd55dd1d0b790f3459e75a38484
ChiliBytes <luis.aguirre.ch90@gmail.com>
2025-09-26 00:15:37 -0600

    feat(python): Add support for Python Scripts execution  - Add the ScriptProcessorService.java class for Python scripts execution  - Add readme to document the Python dependencies installation and VENV creation  - Add the requirements.txt file for Python dependencies installation

---
+++ src/main/java/com/chilibytes/mystify/common/service/ScriptProcessorService.java
@@ -0,0 +1,44 @@
+package com.chilibytes.mystify.common.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@Slf4j
+@Service
+public class ScriptProcessorService {
+
+    public record ScriptComponents(String scriptName, List<String>commands) {
+    }
+
+    public  void executeScript(ScriptComponents scriptComponents) {
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(scriptComponents.commands);
+            // Combine stdout and stderr
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+
+            // Read the script output
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream())
+            );
+
+            String line;
+            log.info("=== Executing {} via Python ===", scriptComponents.scriptName);
+            while ((line = reader.readLine()) != null) {
+                log.info("[PYTHON] " + line);
+            }
+
+            int exitCode = process.waitFor();
+            log.info("Processing {} finished with code {} " ,scriptComponents.scriptName, exitCode);
+
+        } catch (Exception e) {
+            log.error("An Exception has occurred while processing {}: {}" , scriptComponents.scriptName ,e.getMessage());
+        }
+    }
+}
