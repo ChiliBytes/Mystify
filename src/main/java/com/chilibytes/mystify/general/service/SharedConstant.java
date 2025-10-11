c7c27eee613046d82bf995acdf2cb130951033bf
ChiliBytes <luis.aguirre.ch90@gmail.com>
2025-10-11 12:23:41 -0600

    feat(dynamic-text): Add the feature to customize the overlay text  - Add the text customization components for UI  - Add the text customization components for the service layer  - Refactor on some control creation methods

--- src/main/java/com/chilibytes/mystify/general/service/SharedConstant.java
+++ src/main/java/com/chilibytes/mystify/general/service/SharedConstant.java
@@ -1,5 +1,6 @@
 package com.chilibytes.mystify.general.service;
 
+import javafx.scene.paint.Color;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,4 +13,22 @@ public class SharedConstant {
     @Getter
     @Setter
     private static boolean multiLayerImage = false;
+
+    @Getter
+    @Setter
+    private static String globalFontStyle = "";
+
+    @Getter
+    @Setter
+    private static String globalFontFamily = "Arial";
+
+    @Getter
+    @Setter
+    private static int globalFontSize = 12;
+
+    @Getter
+    @Setter
+    private static Color globalFontColor = Color.BLACK;
 }
+
+
